a7f425f7f9619b2647ee978b1acf8195b5bd8883
albre116 <albre116@gmail.com>
2014-12-03 17:00:11 -0600

    correct data

--- Proportional_Odds.R
+++ Proportional_Odds.R
@@ -1,31 +1,31 @@
-##########################################################
-####Example file that fits a simple cumulative logit model
-####This is what the initial paper is based off
-##########################################################
+####################################################
+####Cumulative (proportional odds) logit analysis
+####################################################
+rm(list=ls())
+gc()
+graphics.off()
 library(MASS)
-DATA<-read.csv("Data for Bioinformatics - no Internationalt2.csv")
-####Prep the data
-races<-c("AFA","API","CAU","HIS","UNK")
-m=1:5
-DATA=DATA[which(DATA$Race %in% races[m]),]
-response2<-DATA$Productivity.Group
-response<-ifelse(response2=='A','A',ifelse(response2=='B','A',ifelse(response2=='C','C','D')))
-response<-ordered(response,levels=c("D","C","A"))
-Geno_freq<-DATA$Total.Genotype.Frequency
-Geno_freq[which(Geno_freq==0)]=NA
-summary(Geno_freq)
-pull<-which(is.na(Geno_freq))
-Geno_freq<-Geno_freq[-pull]
-response<-response[-pull]
 
-summary(Geno_freq)
-Geno_freq<-log(Geno_freq)
-summary(Geno_freq)
+###Import Data
+RAW_DATA<-read.csv("Raw_Data.csv")
+DATA<-RAW_DATA[c("RID","Race","Frequency.1",
+                 "Frequency.2","Rank.1","Rank.2",
+                 "Total.Genotype.Frequency","Productivity.Group")]
 
-numvec<-as.numeric(response)
+DATA$Productivity.Group<-ordered(DATA$Productivity.Group,levels=c("D","C","B","A"))
+colnames(DATA)<-c("RID","Race","H1","H2","Rank_H1","Rank_H2","GF","Productivity")
+DATA<-DATA[complete.cases(DATA),]###remove NA's
+DATA<-DATA[DATA$GF!=0 & DATA$H1!=0 & DATA$H2!=0,]
+rm(RAW_DATA)
+
+###Format Predictors to be ln-scale
+DATA$GF<-log(DATA$GF)
+DATA$H1<-log(DATA$H1)
+DATA$H2<-log(DATA$H2)
 
 
-fit<-polr(response~Geno_freq)
+###Try a basic Proportional Odds Model on the genotype Frequencies
+fit<-polr(Productivity~GF,data=DATA)
 summary(fit)
 probs<-fitted(fit)
 display<-round(probs,2)
@@ -35,17 +35,16 @@ pi_preds<-as.data.frame(predict(fit,newdata=data.frame("Geno_freq"=geno_values),
                                 type="probs"))
 class<-predict(fit)
 concordance<-numeric(length(class))
-i=1
-for (i in 1:length(response)){
-  concordance[i]=as.numeric(response[i] %in% class[i])                                   
+for (i in 1:nrow(DATA)){
+  concordance[i]=as.numeric(DATA$Productivity.Group[i] %in% class[i])                                   
 }
 print(round(mean(concordance),3))
 
 
 
 #calculate cutoff values
-Cutoff<-array(2)
-for(i in 1:2){
+Cutoff<-array(3)
+for(i in 1:3){
   CUT<-array(1:10000)
   for(j in 1:10000){
     CUT[j]=pi_preds[j,i+1]>pi_preds[j,i]}
@@ -58,44 +57,35 @@ t=signif(exp(Cutoff),3)
 print(t)
 
 
-plot(numvec~Geno_freq,type="n")
-text(Geno_freq,numvec,labels=as.character(response))
-abline(v=Cutoff[1])
-abline(v=Cutoff[2])
 
 
-
-plot(pi_preds$D~geno_values,xlim=c(min(geno_values),max(geno_values)),ylim=c(0,1),type="l",lwd=2,
+plot(pi_preds$D~geno_values,xlim=c(-30,15),ylim=c(0,1),type="l",lwd=2,
      ylab="Membership Probability",xlab="log(Genotype Frequency)")
 title(main="Probability of Search Productivity Group by Genotype Freq",cex.main=.8)
-lines(pi_preds$C~geno_values,lwd=2,lty=3)
+lines(pi_preds$C~geno_values,lwd=2,lty=2)
+lines(pi_preds$B~geno_values,lwd=2,lty=3)
 lines(pi_preds$A~geno_values,lwd=2,lty=4)
-legend("topleft",legend=c("Group D","Group C", "Group A/B"),lty=c(1:4),lwd=2,cex=.7)
+legend("topleft",legend=c("Group D","Group C","Group B", "Group A"),lty=c(1:4),lwd=2,cex=.7)
+legend("center",legend=c(as.expression(paste("Race: ",races[m], sep="")),as.expression(paste("Worksheet #",wksheet,sep=""))),bty="n")
 abline(v=Cutoff[1])
 abline(v=Cutoff[2])
-
+abline(v=Cutoff[3])
 
 val<-exp(Cutoff[3])+.1*exp(Cutoff[3])
-plot(pi_preds$D~exp(geno_values),xlim=c(0,max(exp(geno_values))/10),ylim=c(0,1),type="l",lwd=2,
+plot(pi_preds$D~exp(geno_values),xlim=c(0,val),ylim=c(0,1),type="l",lwd=2,
      main="Probability of Search Productivity Group by Genotype Freq",cex.main=.8,ylab="Membership Probability",
      xlab="Genotype Frequency")
-lines(pi_preds$C~exp(geno_values),lwd=2,lty=3)
+lines(pi_preds$C~exp(geno_values),lwd=2,lty=2,)
+lines(pi_preds$B~exp(geno_values),lwd=2,lty=3)
 lines(pi_preds$A~exp(geno_values),lwd=2,lty=4)
-legend("topleft",legend=c("Group D","Group C", "Group A/B"),lty=c(1:4),lwd=2,cex=.7)
+legend("topleft",legend=c("Group D","Group C","Group B", "Group A"),lty=c(1:4),lwd=2,cex=.7)
+legend("center",legend=c(as.expression(paste("Race: ",races[m], sep="")),as.expression(paste("Worksheet #",wksheet,sep=""))),bty="n")
 abline(v=exp(Cutoff[1]))
 abline(v=exp(Cutoff[2]))
+abline(v=exp(Cutoff[3]))
 
 
-response<-ordered(response,levels=c("A","C","D"))
-class<-ordered(class,levels=c("A","C","D"))
-counts<-table(response,class)
-tvals<-t(prop.table(counts,1))*100
-round(tvals,1)
-barplot(tvals,main="GF precision",xlab="Productivity group",legend=rownames(tvals))
-
-table(response)
-
-Cutoff[1]
-Cutoff[2]
-exp(Cutoff[1])
-exp(Cutoff[2])
+plot.new()
+legend("center",legend=c(paste("D to C: ",round(Cutoff[1],2),t[1],sep=" "),
+                         paste("C to B: ",round(Cutoff[2],2),t[2],sep="  "),paste("B to A: ",round(Cutoff[3],2),t[3],sep="  "),
+                         paste("Concordance: ",round(mean(concordance),3),sep="")))
