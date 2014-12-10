707014e5969521aa845727e9d82ff128b51533b3
unknown <malbrech@L0502491.NMDP.ORG>
2014-12-10 14:55:51 -0600

    added boosting routine

---
+++ Boosting.R
@@ -0,0 +1,58 @@
+####################################################
+####More General Kernel Method Approaches
+####################################################
+rm(list=ls())
+gc()
+graphics.off()
+if(!require("MASS"))
+  (install.packages("MASS"))
+if(!require("ggplot2"))
+  (install.packages("ggplot2"))
+if(!require("kernlab"))
+  (install.packages("kernlab"))
+if(!require("e1071"))
+  (install.packages("e1071"))
+if(!require("mboost"))
+  (install.packages("mboost"))
+if(!require("dplyr"))
+  (install.packages("dplyr"))
+if(!require("reshape2"))
+  (install.packages("reshape2"))
+
+###Import Data
+RAW_DATA<-read.csv("Raw_Data.csv")
+RAW_DATA$Frequency.1*RAW_DATA$Frequency.2
+permute<-rbinom(nrow(RAW_DATA),1,prob=0.5)==1
+RAW_DATA[permute,]<-RAW_DATA[permute,c(1:6,8,7,10,9,11:13)]
+DATA<-RAW_DATA[c("RID","Race","Frequency.1",
+                 "Frequency.2","Rank.1","Rank.2",
+                 "Total.Genotype.Frequency","Productivity.Group")]
+
+DATA$Productivity.Group<-ordered(DATA$Productivity.Group,levels=c("D","C","B","A"))
+colnames(DATA)<-c("RID","Race","H1","H2","Rank_H1","Rank_H2","GF","Productivity")
+DATA<-DATA[complete.cases(DATA),]###remove NA's
+DATA<-DATA[DATA$GF!=0 & DATA$H1!=0 & DATA$H2!=0,]
+rm(RAW_DATA)
+
+###Format Predictors to be ln-scale
+DATA$GF<-log(DATA$GF)
+DATA$H1<-log(DATA$H1)
+DATA$H2<-log(DATA$H2)
+
+
+####Split Train and Test
+set.seed(1103)
+train_idx<-sample(1:nrow(DATA),floor(nrow(DATA)*0.8))
+logical<-rep(FALSE,nrow(DATA))
+logical[train_idx]<-TRUE
+train_idx<-logical
+test_idx<-!logical
+rm(logical)
+TRAIN<-DATA[train_idx,]
+TEST<-DATA[test_idx,]
+
+
+
+
+
+
