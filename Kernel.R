6b38d762c97b3bda7823f095dc99bc3927fe52d8
unknown <malbrech@L0502491.NMDP.ORG>
2015-01-20 13:57:24 -0600

    fixed weighting problem

--- Kernel.R
+++ Kernel.R
@@ -3,36 +3,6 @@
 ####################################################
 source("data_prep.R")
 
-#####fit a ksvm kernel to output class probabilities (versus call)
-######Fit a simple SVM using the e1071 package
-ww<-table(TRAIN$Productivity)/nrow(TRAIN)
-# ###this shows the effect of loss function
-# weights["A"]=0.97
-# weights["B"]=0.01
-# weights["C"]=0.01
-# weights["D"]=0.01
-
-#ww=c("A"=0.25,"B"=0.25,"C"=0.25,"D"=0.25)
-#TRAIN$Productivity<-as.character(TRAIN$Productivity)
-cost=c(0.001,0.01,1,5,10,100)
-error<-cost
-for (i in 1:length(cost)){
-mod<-ksvm(Productivity~H1+H2+Race,data=TRAIN,kernel="rbfdot",class.weights =NULL,
-              kpar="automatic",cross=3,C=cost[i],prob.model=TRUE)
-print(paste("Loop",i,",C=",cost[i]))
-error[i]<-mod@cross
-}
-
-plot(error,type="b")
-C=cost[which(error==min(error))]
-
-bestmod<-ksvm(Productivity~H1+H2+Race,data=TRAIN,kernel="rbfdot", class.weights=NULL,
-          kpar="automatic",cross=3,C=C,prob.model=TRUE)
-
-
-
-###Plot the linear fit
-######Plot 2-D grid of Classification Rule
 create_grid<-function(x1,x2,n=1000){
   min_x1<-min(x1)
   max_x1<-max(x1)
@@ -48,39 +18,82 @@ grid<-create_grid(DATA$H1,DATA$H2,n=10000)
 
 
 par_disp<-unique(TRAIN$Race)
-race=par_disp[1]
+race=par_disp[3]
 
 
 for(race in par_disp){
+  CUT_TRAIN<-TRAIN[TRAIN$Race==race, ]
+  weights<-100/table(CUT_TRAIN$Productivity)/nrow(CUT_TRAIN)
+  
+  ###now, in this example the class.weights function doesn't work properly
+  ###what is an equivalent solution from the "Loss" function perspective???
+  ###simply sample data in equal proportions to create balanced data
+  nsample<-table(CUT_TRAIN$Productivity)
+  CUT_ADJUST<-data.frame()
+  for(i in names(nsample)){
+    idx<-rep_len(1:nsample[i],max(nsample))
+    TMP<-CUT_TRAIN[CUT_TRAIN$Productivity==i,]
+    CUT_ADJUST<-rbind(CUT_ADJUST,TMP[idx,])
+  }
+    
+  table(CUT_ADJUST$Productivity)
+  
+  
+  cost=c(0.001,0.01,1,5,10,100)
+  error<-cost
+  i=1
+  for (i in 1:length(cost)){
+    mod<-ksvm(Productivity~H1+H2,data=CUT_ADJUST,kernel="rbfdot", class.weights =NULL,
+              kpar="automatic",cross=3,C=cost[i],prob.model=TRUE)
+    print(paste("Loop",i,",C=",cost[i]))
+    error[i]<-mod@cross
+  }
+  
+  plot(error,type="b")
+  C=cost[which(error==min(error))]
+  
+  bestmod<-ksvm(Productivity~H1+H2+Race,data=CUT_ADJUST,kernel="rbfdot", class.weights=NULL,
+                kpar="automatic",cross=3,C=C,prob.model=TRUE)
+  
+    
+  ###do the holdout validation
   CUT_TEST<-TEST[TEST$Race==race, ]
   grid$Race=CUT_TEST$Race[1]
   grid$class<-predict(bestmod,newdata = grid)
+  class<-predict(bestmod,newdata=CUT_TEST)
+  out<-data.frame(table(class,CUT_TEST$Productivity))
+  for(id in unique(out$Var2)){
+    out$Freq[out$Var2==id]<-out$Freq[out$Var2==id]/sum(out$Freq[out$Var2==id])
+  }
+  out<-out[ out$class==out$Var2,]
+  c<-paste0(out$Var2,"=",round(out$Freq,2)*100,"%",collapse="; ")
+  
+  base_layer<-ggplot(data=grid,aes(x=H1,y=H2,colour=class))+
+    ggtitle(paste("Decision Boundaries, Race=",race))+
+    geom_point(size=3.5,alpha=1,shape=15)
+  
+  train_plot<-base_layer+geom_text(data=CUT_TEST,aes(x=H1,y=H2,label=Productivity),size=5,colour="black")+
+    ggtitle(paste("Decision Boundaries for",race,"Percent Correct=",c))+facet_wrap(~Productivity,ncol=2)
+  print(train_plot)
+  
+  ###check out probabilities instead of class calls
+
   func=predict(bestmod,newdata = grid,type ="probabilities" )
   func<-data.frame(grid,func)
   class<-predict(bestmod,newdata=CUT_TEST)
-  concordance<-numeric(length(class))
-  for (i in 1:nrow(CUT_TEST)){
-    concordance[i]=as.numeric(CUT_TEST$Productivity[i] %in% class[i])                                   
-  }
-  correct<-round(mean(concordance),3)*100
+
   
-#   base_layer<-ggplot(data=grid,aes(x=H1,y=H2,colour=class))+
-#     ggtitle(paste("Decision Boundaries, Race=",race))+
-#     geom_point(size=3.5,alpha=1,shape=15)
-#   
-#   train_plot<-base_layer+geom_text(data=CUT_TEST,aes(x=H1,y=H2,label=Productivity),size=5,colour="black")+
-#     ggtitle(paste("Decision Boundaries for",race,"Percent Correct=",correct,"%"))+facet_wrap(~Productivity,ncol=2)
-#   print(train_plot)
-    
   plot_dat<-melt(func,id=c("H1","H2","Race","class"))
   plot<-ggplot(data=plot_dat,aes(x=H1,y=H2,fill=value))+
     geom_tile()+
     stat_contour(data=plot_dat,aes(z=value))+
-    ggtitle(paste("Probability Distribution for",race,"Percent Correct=",correct,"%"))+
+    ggtitle(paste("Probability Distribution for",race,"Percent Correct=",c))+
     facet_wrap(~variable,ncol=2)
   print(plot)  
+  
+    
+  
 }
-
 
 
 
